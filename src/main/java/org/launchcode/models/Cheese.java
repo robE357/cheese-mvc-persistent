a2bfc470f4238e0e56cb7e78ef4b6f7636930801
robE357 <rob.ellis357@gmail.com>
2019-03-11 20:58:19 -0400

    Working Step3

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -1,17 +1,18 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 /**
  * Created by LaunchCode
  */
 @Entity
 public class Cheese {
+
+    @ManyToMany(mappedBy = "cheeses")
+    private List<Menu> menus;
 
     @Id
     @GeneratedValue
